0a6e961dcfefca9b444b1d40c5b48b846649e2d0
Thien Trinh <takatathien@gmail.com>
2022-05-29 01:27:54 -0700

    Added Exit option and grid layout to the board.

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,32 +1,50 @@
 package com.chess.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+import com.chess.engine.board.BoardUtils;
 
 
 public class Table {
 
 	private final JFrame gameFrame;
+	private final BoardPanel boardPanel;
+	private final Color lightTileColor = Color.decode("#FFFACD");
+	private final Color darkTileColor = Color.decode("#593E1A");
 	
-	private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600); 
+	private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+	private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+	private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10); 
 	
 	public Table() {
 		this.gameFrame = new JFrame("JavaChess");
-		final JMenuBar tableMenuBar = new JMenuBar();
-		populateMenuBar(tableMenuBar);
+		this.gameFrame.setLayout(new BorderLayout());
+		final JMenuBar tableMenuBar = createTableMenuBar();
 		this.gameFrame.setJMenuBar(tableMenuBar);
 		this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+		this.boardPanel = new BoardPanel();
+		this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
 		this.gameFrame.setVisible(true);
 	}
 
-	private void populateMenuBar(final JMenuBar tableMenuBar) {
+	private JMenuBar createTableMenuBar() {
+		final JMenuBar tableMenuBar = new JMenuBar();
 		tableMenuBar.add(createFileMenu());
+		return tableMenuBar;
 	}
 
 	private JMenu createFileMenu() {
@@ -40,7 +58,63 @@ public class Table {
 		});
 		fileMenu.add(openPGN);
 		
+		final JMenuItem exitMenuItem = new JMenuItem("Exit");
+		exitMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Exit!");
+				System.exit(0);
+			}
+		});
+		fileMenu.add(exitMenuItem);
+		
 		return fileMenu;
+	}
+	
+	private class BoardPanel extends JPanel {
+		
+		final List<TilePanel> boardTiles;
+		
+		BoardPanel() {
+			super(new GridLayout(8, 8));
+			this.boardTiles = new ArrayList<>();
+			
+			for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+				final TilePanel tilePanel = new TilePanel(this, i);
+				this.boardTiles.add(tilePanel);
+				add(tilePanel);
+			}
+			setPreferredSize(BOARD_PANEL_DIMENSION);
+			validate();
+		}
+		
+	}
+	
+	private class TilePanel extends JPanel {
+		
+		private final int tileId;
+		
+		TilePanel(final BoardPanel boardPanel, final int tileId) {
+			super(new GridBagLayout());
+			this.tileId = tileId;
+			setPreferredSize(TILE_PANEL_DIMENSION);
+			assignTileColor();
+			validate();
+		}
+
+		private void assignTileColor() {
+			if (BoardUtils.FIRST_ROW[this.tileId] ||
+					BoardUtils.THIRD_ROW[this.tileId] ||
+					BoardUtils.FIFTH_ROW[this.tileId] ||
+					BoardUtils.SEVENTH_ROW[this.tileId]) {
+				setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+			} else if (BoardUtils.SECOND_ROW[this.tileId] ||
+					BoardUtils.FOURTH_ROW[this.tileId] ||
+					BoardUtils.SIXTH_ROW[this.tileId] ||
+					BoardUtils.EIGHTH_ROW[this.tileId]) {
+				setBackground(this.tileId % 2 == 0 ? darkTileColor : lightTileColor);
+			}
+		}
 	}
 	
 }
