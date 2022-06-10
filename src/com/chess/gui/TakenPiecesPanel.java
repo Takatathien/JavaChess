90f0aa43e3dbe0e416102dfa21677ecbc96f24ac
Thien Trinh <takatathien@gmail.com>
2022-06-09 23:04:26 -0700

    Added TakenPiecesPanel class.

---
+++ src/com/chess/gui/TakenPiecesPanel.java
@@ -0,0 +1,103 @@
+package com.chess.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+import com.chess.engine.board.Move;
+import com.chess.engine.piece.Piece;
+import com.chess.gui.Table.MoveLog;
+import com.google.common.primitives.Ints;
+
+public class TakenPiecesPanel extends JPanel {
+
+	private final JPanel northPanel;
+	private final JPanel southPanel;
+	
+	private static final Color PANEL_COLOR = Color.decode("0xFDFE6");
+	private static final Dimension TAKEN_PIECES_DIMENSION = new Dimension(40, 80);
+	private static final EtchedBorder PANEL_BORDER = new EtchedBorder(EtchedBorder.RAISED);
+	
+	public TakenPiecesPanel() {
+		super(new BorderLayout());
+		this.setBackground(PANEL_COLOR);
+		this.setBorder(PANEL_BORDER);
+		this.northPanel = new JPanel(new GridLayout(8, 2));
+		this.southPanel = new JPanel(new GridLayout(8, 2));
+		this.northPanel.setBackground(PANEL_COLOR);
+		this.southPanel.setBackground(PANEL_COLOR);
+		this.add(this.northPanel, BorderLayout.NORTH);
+		this.add(this.southPanel, BorderLayout.SOUTH);
+		setPreferredSize(TAKEN_PIECES_DIMENSION);
+	}
+	
+	public void redo(final MoveLog moveLog) {
+		this.northPanel.removeAll();
+		this.southPanel.removeAll();
+		
+		final List<Piece> whiteTakenPieces = new ArrayList<>();
+		final List<Piece> blackTakenPieces = new ArrayList<>();
+		
+		for (final Move move : moveLog.getMove()) {
+			if (move.isAttack()) {
+				final Piece takenPiece = move.getAttackedPiece();
+				
+				if (takenPiece.getPieceAlliance().isWhite()) {
+					whiteTakenPieces.add(takenPiece);
+				} else if (takenPiece.getPieceAlliance().isBlack()) {
+					blackTakenPieces.add(takenPiece);
+				} else {
+					throw new RuntimeException("Should not reach here!");
+				}
+			}
+		}
+		
+		Collections.sort(whiteTakenPieces, new Comparator<Piece>() {
+			@Override
+			public int compare(Piece o1, Piece o2) {
+				return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+			}
+		});
+		
+		Collections.sort(blackTakenPieces, new Comparator<Piece>() {
+			@Override
+			public int compare(Piece o1, Piece o2) {
+				return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+			}
+		});
+		
+		for (final Piece takenPiece : whiteTakenPieces) {
+			try {
+				final BufferedImage image = ImageIO.read(new File("art/pieces/plain" + 
+						takenPiece.getPieceAlliance().toString().substring(0, 1) + "" + 
+						takenPiece.toString()));
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for (final Piece takenPiece : blackTakenPieces) {
+			try {
+				final BufferedImage image = ImageIO.read(new File("art/pieces/plain" + 
+						takenPiece.getPieceAlliance().toString().substring(0, 1) + "" + 
+						takenPiece.toString()));
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		validate();
+	}
+}
