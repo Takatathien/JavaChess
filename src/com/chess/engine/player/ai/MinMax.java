377612694b8b1395558d0ee1833a69de9e32281a
Thien Trinh <takatathien@gmail.com>
2022-06-16 23:17:17 -0700

    Starting to implement Minimax.

---
+++ src/com/chess/engine/player/ai/MinMax.java
@@ -0,0 +1,25 @@
+package com.chess.engine.player.ai;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+public class MinMax implements MoveStrategy{
+
+	private final BoardEvaluator boardEvaluator;
+	
+	public MinMax() {
+		this.boardEvaluator = null;
+	}
+	
+	@Override
+	public String toString() {
+		return "MinMax";
+	}
+	
+	@Override
+	public Move execute(Board board, int depth) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
